676486f61f19580d9d590bcce5f91215d392d49e
Dayon Oliveira <dayon.dos@gmail.com>
2021-05-09 04:03:04 -0300

    New JS file 'transformation' was created.

---
+++ transformation.js
@@ -0,0 +1,72 @@
+class Transformations{
+
+    translate2D(vector, dx, dy){
+
+    }
+
+    translate3D(vector, dx, dy, dz){
+
+    }
+
+    rotation2D(vector, angle){
+
+    }
+
+    rotation3DX(vector, angle){
+
+    }
+
+    rotation3DY(vector, angle){
+
+    }
+
+    rotation3DZ(vector, angle){
+
+    }
+
+    reflection2DX(vector){
+        
+    }
+
+    reflection2DY(vector){
+
+    }
+
+    reflection3DXY(vector){
+
+    }
+
+    reflection3DXZ(vector){
+
+    }
+
+    reflection3DYZ(vector){
+
+    }
+
+    projection2DX(vector){
+
+    }
+
+    projection2DY(vector){
+
+    }
+
+    projection3DX(vector){
+
+    }
+
+    projection3DY(vector){
+
+    }
+
+    projection3DZ(vector){
+
+    }
+
+    shearing(vector, kx, ky){
+        
+    }
+
+
+}
