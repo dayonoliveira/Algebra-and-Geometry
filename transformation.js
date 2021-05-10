b1a8424cda9cde6faa5acd1d2ae190effec221f9
Dayon Oliveira <dayon.dos@gmail.com>
2021-05-10 01:11:38 -0300

    Error treatments have been added.

--- transformation.js
+++ transformation.js
@@ -26,6 +26,12 @@ class Transformations{
 
     reflection2DX(vector){
 
+        if(!(vector instanceof Vector)){
+            throw new Error("The parameter must be an object of type Vector.");
+        }else if(vector.dim != 2){
+            throw new Error("The size of the vector must be 2.");
+        }
+
         let la = new LinearAlgebra();
         let c = new Vector(vector.dim + 1);
         let canonicalMatrix = new Matrix(3, 3, [1, 0, 0, 0, -1, 0, 0, 0, 1]);
@@ -46,6 +52,12 @@ class Transformations{
     }
 
     reflection2DY(vector){
+
+        if(!(vector instanceof Vector)){
+            throw new Error("The parameter must be an object of type Vector.");
+        }else if(vector.dim != 2){
+            throw new Error("The size of the vector must be 2.");
+        }
 
         let la = new LinearAlgebra();
         let c = new Vector(vector.dim + 1);
@@ -68,14 +80,83 @@ class Transformations{
 
     reflection3DXY(vector){
 
+        if(!(vector instanceof Vector)){
+            throw new Error("The parameter must be an object of type Vector.");
+        }else if(vector.dim != 3){
+            throw new Error("The size of the vector must be 3.");
+        }
+
+        let la = new LinearAlgebra();
+        let c = new Vector(vector.dim + 1);
+        let canonicalMatrix = new Matrix(4, 4, [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, -1, 0, 0, 0, 0, 1]);
+
+        for(let i = 1; i <= c.dim; i++){
+            if(i == c.dim){
+                c.set(i, 1);
+            }else{
+                c.set(i, vector.get(i));
+            }
+        }
+
+        c = la.dot(canonicalMatrix,c);
+
+        c.elements.pop();
+
+        return c;
     }
 
     reflection3DXZ(vector){
+        
+        if(!(vector instanceof Vector)){
+            throw new Error("The parameter must be an object of type Vector.");
+        }else if(vector.dim != 3){
+            throw new Error("The size of the vector must be 3.");
+        }
 
+        let la = new LinearAlgebra();
+        let c = new Vector(vector.dim + 1);
+        let canonicalMatrix = new Matrix(4, 4, [1, 0, 0, 0, 0, -1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]);
+
+        for(let i = 1; i <= c.dim; i++){
+            if(i == c.dim){
+                c.set(i, 1);
+            }else{
+                c.set(i, vector.get(i));
+            }
+        }
+
+        c = la.dot(canonicalMatrix,c);
+
+        c.elements.pop();
+
+        return c;
     }
 
     reflection3DYZ(vector){
 
+        if(!(vector instanceof Vector)){
+            throw new Error("The parameter must be an object of type Vector.");
+        }else if(vector.dim != 3){
+            throw new Error("The size of the vector must be 3.");
+        }
+
+        let la = new LinearAlgebra();
+        let c = new Vector(vector.dim + 1);
+        let canonicalMatrix = new Matrix(4, 4, [-1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]);
+
+        for(let i = 1; i <= c.dim; i++){
+            if(i == c.dim){
+                c.set(i, 1);
+            }else{
+                c.set(i, vector.get(i));
+            }
+        }
+
+        c = la.dot(canonicalMatrix,c);
+
+        c.elements.pop();
+
+        return c;
     }
 
     projection2DX(vector){
