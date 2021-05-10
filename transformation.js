73e10e2c2605265f5b09b37e6a740f4171b7168a
Dayon Oliveira <dayon.dos@gmail.com>
2021-05-10 00:36:12 -0300

    The 2D reflection methods have been finalized.

--- transformation.js
+++ transformation.js
@@ -25,11 +25,45 @@ class Transformations{
     }
 
     reflection2DX(vector){
-        
+
+        let la = new LinearAlgebra();
+        let c = new Vector(vector.dim + 1);
+        let canonicalMatrix = new Matrix(3, 3, [1, 0, 0, 0, -1, 0, 0, 0, 1]);
+
+        for(let i = 1; i <= c.dim; i++){
+            if(i == c.dim){
+                c.set(i, 1);
+            }else{
+                c.set(i, vector.get(i));
+            }
+        }
+
+        c = la.dot(canonicalMatrix,c);
+
+        c.elements.pop();
+
+        return c;
     }
 
     reflection2DY(vector){
 
+        let la = new LinearAlgebra();
+        let c = new Vector(vector.dim + 1);
+        let canonicalMatrix = new Matrix(3, 3, [-1, 0, 0, 0, 1, 0, 0, 0, 1]);
+
+        for(let i = 1; i <= c.dim; i++){
+            if(i == c.dim){
+                c.set(i, 1);
+            }else{
+                c.set(i, vector.get(i));
+            }
+        }
+
+        c = la.dot(canonicalMatrix,c);
+
+        c.elements.pop();
+
+        return c;
     }
 
     reflection3DXY(vector){
