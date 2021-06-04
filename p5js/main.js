b17dc965bced936f194e02b25b3ffb41cc496e1e
Dayon Oliveira <dayon.dos@gmail.com>
2021-06-03 22:03:19 -0300

    I finished the class Rectangle.

--- p5js/main.js
+++ p5js/main.js
@@ -2,14 +2,23 @@ let line = new Line(0, 20, 80, 20);
 let rectangle = new Rectangle(20, 20, 40, 40);
 
 function setup(){
-    createCanvas(1920, 940);
+    createCanvas(1920, 940, WEBGL);
     background(52);
     frameRate(2);
+
+    rectangle.draw();
+
+    
+    
 }
 
 function draw(){
     
     rectangle.draw();
+
+    //rectangle.rotate(40);
+
+    //rectangle.translate(60, 60);
     
     //line.draw();
 
--- p5js/shapes2d.js
+++ p5js/shapes2d.js
@@ -18,6 +18,7 @@ class Line{
         let tr = new Transformations();
         let aux1 = new Vector(2, [this.points[0].get(1), this.points[0].get(2)]);
         let aux2 = new Vector(2, [this.points[1].get(1), this.points[1].get(2)]);
+
         aux1 = tr.rotation2D(aux1, angle);
         aux2 = tr.rotation2D(aux2, angle);
 
@@ -32,6 +33,7 @@ class Line{
         let tr = new Transformations();
         let aux1 = new Vector(2, [this.points[0].get(1), this.points[0].get(2)]);
         let aux2 = new Vector(2, [this.points[1].get(1), this.points[1].get(2)]);
+
         aux1 = tr.translate2D(aux1, x, y);
         aux2 = tr.translate2D(aux2, x, y);
 
@@ -64,6 +66,52 @@ class Rectangle{
         ]
     }
 
+    rotate(angle){
+        let tr = new Transformations();
+        let aux1 = new Vector(2, [this.points[0].get(1), this.points[0].get(2)]);
+        let aux2 = new Vector(2, [this.points[1].get(1), this.points[1].get(2)]);
+        let aux3 = new Vector(2, [this.points[2].get(1), this.points[2].get(2)]);
+        let aux4 = new Vector(2, [this.points[3].get(1), this.points[3].get(2)]);
+
+        aux1 = tr.rotation2D(aux1, angle);
+        aux2 = tr.rotation2D(aux2, angle);
+        aux3 = tr.rotation2D(aux3, angle);
+        aux4 = tr.rotation2D(aux4, angle);
+
+        let vector1 = new Vector(aux1.rows, [aux1.elements[0], aux1.elements[1]]);
+        let vector2 = new Vector(aux2.rows, [aux2.elements[0], aux2.elements[1]]);
+        let vector3 = new Vector(aux3.rows, [aux3.elements[0], aux3.elements[1]]);
+        let vector4 = new Vector(aux4.rows, [aux4.elements[0], aux4.elements[1]]);
+
+        this.points[0] = vector1;
+        this.points[1] = vector2;
+        this.points[2] = vector3;
+        this.points[3] = vector4;
+    }
+
+    translate(x, y){
+        let tr = new Transformations();
+        let aux1 = new Vector(2, [this.points[0].get(1), this.points[0].get(2)]);
+        let aux2 = new Vector(2, [this.points[1].get(1), this.points[1].get(2)]);
+        let aux3 = new Vector(2, [this.points[2].get(1), this.points[2].get(2)]);
+        let aux4 = new Vector(2, [this.points[3].get(1), this.points[3].get(2)]);
+
+        aux1 = tr.translate2D(aux1, x, y);
+        aux2 = tr.translate2D(aux2, x, y);
+        aux3 = tr.translate2D(aux3, x, y);
+        aux4 = tr.translate2D(aux4, x, y);
+
+        let vector1 = new Vector(aux1.rows, [aux1.elements[0], aux1.elements[1]]);
+        let vector2 = new Vector(aux2.rows, [aux2.elements[0], aux2.elements[1]]);
+        let vector3 = new Vector(aux3.rows, [aux3.elements[0], aux3.elements[1]]);
+        let vector4 = new Vector(aux4.rows, [aux4.elements[0], aux4.elements[1]]);
+
+        this.points[0] = vector1;
+        this.points[1] = vector2;
+        this.points[2] = vector3;
+        this.points[3] = vector4;
+    }
+
     draw(){
         noFill()
         stroke(color(255));
@@ -71,11 +119,12 @@ class Rectangle{
         vertex(this.points[0].get(1), this.points[0].get(2));
         vertex(this.points[1].get(1), this.points[1].get(2));
         vertex(this.points[2].get(1), this.points[2].get(2));
-        
         vertex(this.points[0].get(1), this.points[0].get(2));
         vertex(this.points[3].get(1), this.points[3].get(2));
-        vertex(this.points[2].get(1), this.points[3].get(2));
+        vertex(this.points[2].get(1), this.points[2].get(2));
         endShape();
+
+
     }
 
 }
