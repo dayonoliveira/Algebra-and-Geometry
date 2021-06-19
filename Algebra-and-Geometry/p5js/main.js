0d530b3ae445095bccd61c80fabe51bf6ba97533
Dayon Oliveira <dayon.dos@gmail.com>
2021-06-19 19:51:38 -0300

    Reorganized folders and files.

--- Algebra-and-Geometry/p5js/main.js
+++ Algebra-and-Geometry/p5js/main.js
@@ -1,6 +1,6 @@
 let line = new Line(0, 20, 80, 20);
 let rectangle = new Rectangle(20, 20, 40, 40);
-let circle = new Circle(0, 0, 60, 10);
+let circle = new Circle(0, 0, 60, 20);
 let triangle = new Triangle(0, 0, 30, 0, 15, 60);
 
 let plane = new Plane(20,20,20,40,40,0);
@@ -17,6 +17,8 @@ function setup(){
 function draw(){
 
     background(52);
+
+    circle.draw();
 
 
     /* # shapes2d tests #
