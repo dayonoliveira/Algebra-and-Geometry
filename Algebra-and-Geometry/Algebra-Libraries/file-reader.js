f3ec6f45124e12c4fbd9c1c1b11fd929061f6382
Dayon Oliveira <dayon.dos@gmail.com>
2021-08-07 17:48:10 -0300

    Some of the logic has changed and data is being sent to HTML elements.

--- Algebra-and-Geometry/Algebra-Libraries/file-reader.js
+++ Algebra-and-Geometry/Algebra-Libraries/file-reader.js
@@ -1,6 +1,7 @@
 let userFiles = document.getElementById('userFiles');
 let textarea1 = document.getElementById('textarea1');
 let textarea2 = document.getElementById('textarea2');
+let exeTime = document.getElementById('exeTime');
 
 function findSymbol(a){
     let newVet = [];
@@ -14,28 +15,6 @@ function findSymbol(a){
     }
     
     return newVet;
-}
-
-function linesVector(a){
-    let vetAux = [];
-    let vetAux2 = [];
-
-    vetAux.push(a[0].split(" "));
-
-    vetAux2[0] = parseInt(vetAux[0][0]);
-
-    return vetAux2;
-}
-
-function columnsVector(a){
-    let vetAux = [];
-    let vetAux2 = [];
-
-    vetAux.push(a[0].split(" "));
-
-    vetAux2[0] = parseInt(vetAux[0][1]);
-
-    return vetAux2;
 }
 
 function elementVector(a){
@@ -69,19 +48,16 @@ userFiles.addEventListener('change', () => {
         let matrixLines = fileMatrix[0].split(" ")[0];
         let matrixColumns = fileMatrix[0].split(" ")[1];
         let elements = elementVector(fileMatrix);
-        let matrixObject = new Matrix(linhas[0], colunas[0], elements);
+        let matrixObject = new Matrix(matrixLines, matrixColumns, elements);
         let la = new LinearAlgebra();
-        //console.time('la.gauss("a")');
-        //let b = la.gauss(a);
-        //console.timeEnd('la.gauss("a")');
-        //let resultados = [];
-        //console.time('la.solve("a")');
-        resultados = la.solve(a);
-        //console.timeEnd('la.solve("a")');
-        textarea1.value = fileLines.join('\n');
-        console.log(resultados);
-        //console.log("pronto!");
+        let time = performance.now();
+        let results = la.solve(matrixObject).elements;
+        let timeEnd = performance.now();
+        
+        exeTime.innerHTML = `Exe. time (ms): ${(timeEnd - time).toFixed(2)}`;
 
+        textarea1.value = fileLines.join('\n');
+        textarea2.value = "Approximate results\n\n" + results.join('\n');
     };
  
     reader.onerror = (e) => alert(e.target.error.name);
