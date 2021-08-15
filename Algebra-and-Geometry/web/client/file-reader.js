777d5c591ead84867cec5c356902d43f445f9d8a
Dayon Oliveira <dayon.dos@gmail.com>
2021-08-15 19:40:35 -0300

    Now this file is responsible for making requests to node.

--- Algebra-and-Geometry/web/client/file-reader.js
+++ Algebra-and-Geometry/web/client/file-reader.js
@@ -2,35 +2,7 @@ let userFiles = document.getElementById('userFiles');
 let textarea1 = document.getElementById('textarea1');
 let textarea2 = document.getElementById('textarea2');
 let exeTime = document.getElementById('exeTime');
-
-function findSymbol(a){
-    let newVet = [];
-    let counter = 0;
-
-    for(let i = 0; i < a.length - 1; i++){
-        if(!a[i].startsWith("%")){
-            newVet[counter] = a[i];
-            counter++
-        }
-    }
-    
-    return newVet;
-}
-
-function elementVector(a){
-    let vetAux = [];
-    let vetAux2 = [];
-
-    for(let i = 0; i < a.length; i++){
-        vetAux.push(a[i].split(" "));
-    }
-
-    for(let i = 1; i < vetAux.length; i++){
-        vetAux2[i - 1] = parseInt(vetAux[i][2]);
-    }
-
-    return vetAux2;
-}
+let fileData = new FileData();
 
 userFiles.addEventListener('change', () => {
     let files = userFiles.files;
@@ -44,20 +16,26 @@ userFiles.addEventListener('change', () => {
     reader.onload = (e) => {
         const file = e.target.result;
         const fileLines = file.split(/\r\n|\n/);
-        let fileMatrix = findSymbol(fileLines);
+        let fileMatrix = fileData.findSymbol(fileLines);
         let matrixLines = fileMatrix[0].split(" ")[0];
         let matrixColumns = fileMatrix[0].split(" ")[1];
-        let elements = elementVector(fileMatrix);
-        let matrixObject = new Matrix(matrixLines, matrixColumns, elements);
-        let la = new LinearAlgebra();
-        let time = performance.now();
-        let results = la.solve(matrixObject).elements;
-        let timeEnd = performance.now();
-        
-        exeTime.innerHTML = `Exe. time (ms): ${(timeEnd - time).toFixed(2)}`;
+        let elements = fileData.elementVector(fileMatrix);
+        let requestObject = {
+            method: "",
+            matrixLines: matrixLines,
+            matrixColumns: matrixColumns,
+            matrixData: elements.toString()
+        }
 
+        $.post(
+            "http://localhost:8080/GaussJordan",
+            requestObject,
+            (data) => {
+                exeTime.innerHTML = `Exe. time (ms): ${data.time}`;
+                textarea2.value = "Approximate results\n\n" + data.results.join('\n');
+            }
+        )
         textarea1.value = fileLines.join('\n');
-        textarea2.value = "Approximate results\n\n" + results.join('\n');
     };
  
     reader.onerror = (e) => alert(e.target.error.name);
