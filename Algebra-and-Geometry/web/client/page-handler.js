778fab67b2ed04d20e5c5f683b1eaf1e5d728aee
Dayon Oliveira <dayon.dos@gmail.com>
2021-09-07 14:01:52 -0300

    Routes are already working. All that's left is to make a few changes.

--- Algebra-and-Geometry/web/client/page-handler.js
+++ Algebra-and-Geometry/web/client/page-handler.js
@@ -1,4 +1,9 @@
 $(()=>{
+
+    let lastMethod = ""
+
+    $(".inputMethod").hide()
+
     // Switch to P5JS page.
 
     $("#switchPageButton").on("click", ()=>{
@@ -8,39 +13,78 @@ $(()=>{
     // Sidebar options redirects.
 
     $("#determinant").on("click", ()=>{
-        $("main").load("method-dad.html #methodDadMainContentDeterminant");
+        $(".home").hide()
+        $(".inputMethod.methodManualMainContent").hide()
+        $(".inputMethod.methodDadMainContent").show()
+        $("#dadMethod").text("Determinant")
     })
 
     $("#dot").on("click", ()=>{
-        $("main").load("method-dad.html #methodDadMainContentDot");
+        $(".home").hide()
+        $(".inputMethod.methodManualMainContent").hide()
+        $(".inputMethod.methodDadMainContent").show()
+        $("#dadMethod").text("Dot")
     })
 
     $("#inverse").on("click", ()=>{
-        $("main").load("method-dad.html #methodDadMainContentInverse");
+        $(".home").hide()
+        $(".inputMethod.methodManualMainContent").hide()
+        $(".inputMethod.methodDadMainContent").show()
+        $("#dadMethod").text("Inverse")
     })
 
     $("#gauss").on("click", ()=>{
-        $("main").load("method-dad.html #methodDadMainContentGauss");
+        $(".home").hide()
+        $(".inputMethod.methodManualMainContent").hide()
+        $(".inputMethod.methodDadMainContent").show()
+        $("#dadMethod").text("Gauss")
     })
 
     $("#gaussJordan").on("click", ()=>{
-        $("main").load("method-dad.html #methodDadMainContentGaussJordan");
+        $(".home").hide()
+        $(".inputMethod.methodManualMainContent").hide()
+        $(".inputMethod.methodDadMainContent").show()
+        $("#dadMethod").text("Gauss-Jordan")
     })
 
     $("#sum").on("click", ()=>{
-        $("main").load("method-dad.html #methodDadMainContentSum");
+        $(".home").hide()
+        $(".inputMethod.methodManualMainContent").hide()
+        $(".inputMethod.methodDadMainContent").show()
+        $("#dadMethod").text("Sum")
     })
 
     $("#times").on("click", ()=>{
-        $("main").load("method-dad.html #methodDadMainContentTimes");
+        $(".home").hide()
+        $(".inputMethod.methodManualMainContent").hide()
+        $(".inputMethod.methodDadMainContent").show()
+        $("#dadMethod").text("Times")
     })
 
     $("#transpose").on("click", ()=>{
-        $("main").load("method-dad.html #methodDadMainContentTranspose");
+        $(".home").hide()
+        $(".inputMethod.methodManualMainContent").hide()
+        $(".inputMethod.methodDadMainContent").show()
+        $("#dadMethod").text("Transpose")
     })
 
     // Manual option redirection.
 
+    $("#manualChoice").on("click", ()=>{
+        $(".home").hide()
+        $(".inputMethod.methodDadMainContent").hide()
+        $(".inputMethod.methodManualMainContent").show()
+        $("#manualMethod").text($("#dadMethod").text())
+    })
+
+    // File option redirection.
+
+    $("#fileChoice").on("click", ()=>{
+        $(".home").hide()
+        $(".inputMethod.methodManualMainContent").hide()
+        $(".inputMethod.methodDadMainContent").show()
+        $("#manualMethod").text($("#dadMethod").text())
+    })
 
 
     // Redirection of the file option.
@@ -54,8 +98,10 @@ $(()=>{
     })
 
     // Redirection to manual data entry tutorial.
-
-
+    
+    $("#manualTutorialLink").on("click", ()=>{
+        $(location).attr("href", "tutorial-manual.html")
+    })
 
     // Result page redirection.
 
